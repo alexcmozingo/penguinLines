b05bf699b8c275a790004c6f341401e51afc7b33
Alex Mozingo <alexmozingo@Alexs-MacBook-Air.local>
2020-09-28 11:07:07 -0400

    Lines

--- js/lines.js
+++ js/lines.js
@@ -8,7 +8,56 @@
 var drawLines = function(penguins,target,
                          xScale,yScale)
 {
+   var lineGenerator = d3.line()
+        .x(function(quiz)
+                {
+            return xScale(quiz.day);
+        })
+        .y(function(quiz)
+                {
+            return yScale(quiz.grade);
+        })
    
+   var lines = d3.select("svg")
+        .select("#graph")
+        .selectAll("g")
+        .data(penguins)
+        .enter()
+        .append("g")
+        .classed("line", true)
+   
+        .on("mouseover", function(penguin)
+                {
+                var xPos = d3.event.pageX;
+                var yPos = d3.event.pageY;
+            if(! d3.select(this).classed("off"))
+                {
+                    var xPos = d3.event.pageX;
+                    var yPos = d3.event.pageY;
+                    
+                    d3.selectAll(".line")
+                        .classed("selected", false)
+                    d3.select(this)
+                        .classed("selected", true)
+                        .raise()
+                    d3.select("#tooltip")
+                        .classed("hidden", false)
+                        .style("top",yPos+"px")
+                        .style("left",xPos+"px")
+                    d3.select("img")
+                    .attr("src", "imgs/" + penguin.picture)
+                    
+                }
+        })
+        
+        
+   lines.append("path")
+        .datum(function(penguin)
+               {
+                console.log("function")
+           return penguin.quizes
+   })
+    .attr("d", lineGenerator)
 }
 
 
@@ -24,8 +73,17 @@ var makeTranslateString = function(x,y)
 var drawAxes = function(graphDim,margins,
                          xScale,yScale)
 {
-   
- 
+    var xAxis = d3.axisBottom(xScale)
+    var yAxis = d3.axisLeft(yScale)
+    
+    var axes = d3.select("svg")
+        .append("g")
+    axes.append("g")
+        .attr("transform", "translate("+margins.left+", "+(margins.top+graphDim.height)+")")
+        .call(xAxis)
+    axes.append("g")
+        .attr("transform", "translate("+(margins.left)+", "+margins.top+")")
+        .call(yAxis)
 }
 
 
@@ -33,6 +91,22 @@ var drawAxes = function(graphDim,margins,
 //margins - object that stores the size of the margins
 var drawLabels = function(graphDim,margins)
 {
+    var labels = d3.select("svg")
+        .append("g")
+        .classed("labels", true)
+    
+    labels.append("text")
+        .text("Day of Quiz")
+        .classed("label", true)
+        .attr("text-anchor", "middle")
+        .attr("x", margins.left+(graphDim.width/2))
+        .attr("y", margins.top+graphDim.height+35)
+    
+    labels.append("text")
+        .text("Quiz Grade")
+        .classed("label", true)
+        .attr("text-anchor", "middle")
+        .attr("transform","translate("+(20)+", "+(margins.top+(graphDim.height/2))+") rotate(270)")
     
 }
 
@@ -45,7 +119,7 @@ var initGraph = function(penguins)
     //size of screen
     var screen = {width:800,height:600}
     //how much space on each side
-    var margins = {left:30,right:20,top:20,bottom:30}
+    var margins = {left:45,right:20,top:20,bottom:50}
     
     
     
@@ -62,7 +136,7 @@ var initGraph = function(penguins)
     
     var target = d3.select("svg")
     .append("g")
-    .attr("id","#graph")
+    .attr("id","graph")
     .attr("transform",
           "translate("+margins.left+","+
                         margins.top+")");
